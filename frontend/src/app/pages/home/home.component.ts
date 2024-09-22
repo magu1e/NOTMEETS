302c7741cd65338cf1b7a64b7734502f3a04c9e8
mfarinas <maguifar@gmail.com>
2024-09-22 20:38:58 -0300

    [FRONT] validaciones de los desplegables de horario

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -16,7 +16,6 @@ interface Rooms {
   [key: string]: any
 }
 
-
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -27,11 +26,12 @@ interface Rooms {
 
 
 export class HomeComponent {
-  //roomsMock = roomsMock; // guardar la peticion del back cuando este
   roomsMock: Rooms[] = roomsMock;
   roomFilters!: FormGroup;
-  schedules = ['09:00hs','10:00hs','11:00hs','12:00hs','13:00hs','14:00hs','15:00hs','16:00hs','17:00hs','18:00hs']
   selectedRooms: Rooms[] = [];
+  schedules = ['09:00hs', '10:00hs', '11:00hs', '12:00hs', '13:00hs', '14:00hs', '15:00hs', '16:00hs', '17:00hs', '18:00hs'];
+  filteredEndSchedules: string[] = [...this.schedules];
+  filteredStartSchedules: string[] = [...this.schedules];
 
 
   constructor(private formBuilder: FormBuilder, private apiService: ApiService) {
@@ -41,41 +41,49 @@ export class HomeComponent {
       endTime: ['', [Validators.required]],
       capacity: ['', [Validators.required]],
     });
-    this.addSelectedTimeToRooms() 
-  }
 
-  //Filtros
-  // getValues() {
-  //   this.searchRooms.get('date')?.valueChanges.subscribe((valor) => {
-  //   })
-  // }
-
-
-  //Actualiza el valor del horario elegido en el dropdown
-  selectTime(room: Rooms, time:string, hour: string) {
-      if (time === 'start') {
-        return room['selectedStartTime'] = hour;
-      } else {
-        return room['selectedEndTime'] = hour;
-      }
-  }
-
-  // Agrega selectedStartTime y selectedEndTime a la sala para manejar los desplegables
-  addSelectedTimeToRooms() {
     this.roomsMock.forEach((room) => {
-      room['selectedStartTime'] = '9:00hs';
-      room['selectedEndTime'] = '9:00hs';
-      console.log(room['selectedStartTime'])
-      console.log(room['selectedEndTime'])
+      room['selectedStartTime'] = '';
+      room['selectedEndTime'] = '';
     });
   }
 
-  // addRoomToBooking(room: Rooms) {
-  //   this.selectedRooms.push(room)
-  //   console.log(this.selectedRooms);
-  // }
+
+  // Recorta opciones superiores al horario de fin
+  updateStartOptions(hour: string) {
+    const endIndex = this.schedules.indexOf(hour);
+    return this.filteredStartSchedules = this.schedules.slice(0, endIndex + 1);
+  }
+
+  // Recorta opciones anteriores a la hora de inicio
+  updateEndOptions(hour: string) {
+    const startIndex = this.schedules.indexOf(hour);
+    return this.filteredEndSchedules = this.schedules.slice(startIndex);
+  }
+
+  selectTime(room: Rooms, time: string, hour: string) {
+    let startDate = room['selectedStartTime'];
+    let endDate = room['selectedEndTime'];
+    if (time === 'start') {
+      room['selectedStartTime'] = hour; // Actualiza el horario de inicio seleccionado
+      return this.updateEndOptions(hour); 
+    } else {
+      room['selectedEndTime'] = hour; // Actualiza el horario de fin seleccionado
+
+      //Valida que la startDate no sea superior a endDate (como se recortan las opciones solo pasa cuando startDate y enDate son iguales)
+      if (startDate && endDate && startDate >= endDate) { 
+        console.log('La fecha de inicio debe ser superior a la de fin')
+        //this.toastService.showToast('La hora de inicio no puede ser igual a la de fin', 'error'); //Implementar toast
+      }
+      return this.updateStartOptions(hour);
+    }
+  }
+
+  addRoomToBooking(room: Rooms) {
+    console.log(this.selectedRooms);
+    return this.selectedRooms.push(room);
+  }
 
   onSubmit() {
-  };
-
+  }
 }
