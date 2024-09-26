1ed96fe2d8e70765217f6da4abad20b5b1a9cc34
Leranddy <martinezcl.1814@gmail.com>
2024-09-26 15:37:52 -0300

    Agrego roomcontroller y roomwithbookingsdto

---
+++ backend/Controllers/RoomController.cs
@@ -0,0 +1,37 @@
+﻿using backend.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoomController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public RoomController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Room
+        [HttpGet]
+        public ActionResult<RoomWithBookingsDTO> GetAll(int Roomid)
+        {
+            var allbooking = _context.Bookings.Include(x => x.Room).Where(b=>b.Room.Id==Roomid).ToList();
+            var roomDTo = new RoomWithBookingsDTO(){ 
+            RoomId = allbooking[0].Room.Id,
+            //agregar las demas propiedades del booking conusltar cuales y orden a Magui 
+            Bookings = allbooking.Select(b => new BookingDTO() {Priority =b.Priority }).ToList()
+            
+            };
+
+            return Ok(roomDTo);
+        }
+
+    }
+
+
+}
---
+++ backend/DTOs/RoomWithBookingsDTO.cs
@@ -0,0 +1,20 @@
+﻿using backend.Controllers;
+
+namespace backend.DTOs
+{
+    public class RoomWithBookingsDTO
+    {
+        public int RoomId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Capacity { get; set; }
+        public List<BookingDTO> Bookings { get; set; }
+
+
+
+
+
+
+
+    }
+}
